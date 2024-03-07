4ec54c6b354653b841dc808898bebe0154fc8add
luiscruzcwb <luiscruzcwb@outlook.com>
2024-03-07 13:34:09 -0300

    Add files

--- Terraform-Essentials/main.tf
+++ Terraform-Essentials/main.tf
@@ -1,3 +1,8 @@
+# blocos
+# argumentos
+# hcl 
+
+
 provider "aws" {
   region  = "us-east-1"
   version = "~> 2.0"
@@ -6,7 +11,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    bucket = "terraform-essentials"
+    bucket = "terraform-essentials-luiscruz"
     key    = "terraform-test.tfstate"
     region = "us-east-1"
   }
