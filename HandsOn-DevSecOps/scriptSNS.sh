e5dd4335dd4faf0526a2be6eea7c34256fe3fdb8
Luis Cruz <contato@luiscruz.com.br>
2024-07-02 18:12:20 -0300

    update HandsOn-DevSecOps

--- HandsOn-DevSecOps/scriptSNS.sh
+++ HandsOn-DevSecOps/scriptSNS.sh
@@ -1,8 +1,8 @@
+#AWS Systems Manager
+
 sudo wget -q https://tcb-bootcamps.s3.amazonaws.com/bootcamp-aws/pt/module5/install_security_agent.sh -P /tmp
 sudo chmod +x /tmp/install_security_agent.sh
 sudo /tmp/install_security_agent.sh
 ls -ltr /usr/bin/security_agent
 
 
-
-# SNS arn:aws:sns:us-east-1:339713184934:DevOpsNotification
